2eb7f64cd02223a7c15ce2d7898ae31fec8e65b1
AfalpHy <736353503@qq.com>
2026-04-18 21:23:31 +0800

    feat: enable equalizer support for FLAC

--- nix/overlays/xcode.nix
+++ nix/overlays/xcode.nix
@@ -1,10 +1,9 @@
 final: prev: {
-  darwin = prev.darwin.overrideScope (
-    final: prev: {
-      xcode_16_1 = prev.xcode.overrideAttrs (prev: {
-        outputHash = "sha256-1jyRJVyOmGA7fxRwBnxSJatnOFDu01RJ9aAQXJNuWBw=";
-      });
-      xcode = final.xcode_16_1;
-    }
-  );
+  darwin = prev.darwin.overrideScope (final: prev: {
+    xcode_12_3 = prev.xcode_12_3.overrideAttrs (_: {
+      outputHash = "sha256-CYU2fAeT+DWiK/mpRoGv57RjGfseL23BDU57SokPjk8=";
+    });
+
+    xcode = final.xcode_12_3;
+  });
 }
